7cccf874720da15c682deebce0923d159bce0e20
Andrii <maystrenko22a@gmail.com>
2021-11-03 19:04:38 +0200

    finish

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FeedbackOptions from './FeedbackOptions';
-import s from './Feedback.module.css';
+import Notification from '../Notification';
+import Statistics from '../Statistics';
+import Section from '../Section';
 
 export default class Feedback extends Component {
   static defaultProps = {};
@@ -9,44 +11,49 @@ export default class Feedback extends Component {
     good: 0,
     neutral: 0,
     bad: 0,
-    total: 0,
-    positivePercentage: 0,
   };
   handleOnFeedback = e => {
     this.setState(preventState => ({
       [e.target.name]: preventState[e.target.name] + 1,
     }));
-    this.countTotalFeedback(this.state);
+    this.countTotalFeedback();
     this.countPositiveFeedbackPercentage(this.state);
   };
-  countTotalFeedback = ({ good, neutral, bad }) => {
-    this.setState({ total: good + neutral + bad });
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
   };
   countPositiveFeedbackPercentage = ({ good, total }) => {
-    this.setState({
-      positivePercentage: total === 0 ? 0 : parseInt((100 * good) / total),
-    });
+    const positivePercentage = total === 0 ? 0 : parseInt((100 * good) / total);
+    return positivePercentage;
   };
   render() {
-    const { good, neutral, bad, total, positivePercentage } = this.state;
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage({
+      good,
+      total,
+    });
     return (
       <>
-        <h1 className={s.titleFeedback}>Please leave feedback</h1>
-        <FeedbackOptions
-          // btnName={{ good, neutral, bad }}
-          onOptinClick={this.handleOnFeedback}
-        />
-        <ul className="listStat">
-          <h2 className="titleStat">Statistics</h2>
-          <li className={s.listItem}>Good:{good}</li>
-          <li className={s.listItem}>Neutral:{neutral}</li>
-          <li className={s.listItem}>Bad:{bad}</li>
-          <li className={s.listItem}>Total:{total}</li>
-          <li className={s.listItem}>
-            Positive feedback:{positivePercentage}%
-          </li>
-        </ul>
+        <Section title="Please leave feedback">
+          <FeedbackOptions onOptinClick={this.handleOnFeedback} />
+        </Section>
+        {total === 0 ? (
+          <Notification message="No feedback given" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        )}
       </>
     );
   }
 }
+//
